0f128109abbedc11fb78262aafc4f31624cde2cb
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-02-05 11:47:15 -0800

    got it but my way

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,47 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleUsernameChange = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.username != '' && this.state.passwod != '') {
+      this.props.handleLogin(this.state)
+    } else {
+      console.log('this isnt working dingus')
+    }
   }
 
   render() {
+    console.log(this.state)
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={event => this.handleUsernameChange(event)} id="username" name="username" type="text" value = {this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={event => this.handlePasswordChange(event)} id="password" name="password" type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,34 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "Your Tweet",
+
+    };
+  }
+
+  handleTweetChange = (event) => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  remainingChars = () => {
+    let max = this.props.maxChars
+    let message = this.state.message
+
+    return max - message.length
   }
 
   render() {
+    console.log(this.remainingChars())
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <form>
+        <input onChange={event => this.handleTweetChange(event)} type="text" name="message" id="message" value={this.state.message}/>
+        </form>
+        <p>{this.remainingChars()}</p> 
       </div>
     );
   }
